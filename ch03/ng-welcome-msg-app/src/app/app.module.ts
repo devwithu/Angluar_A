9c5a1e1dba4f94940a3a10eb44ce98b51be0fb7d
DaeJin Jeong <devwithu@naver.com>
2017-08-29 11:54:34 +0900

    Page 66

--- ch03/ng-welcome-msg-app/src/app/app.module.ts
+++ ch03/ng-welcome-msg-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeMsgComponent
   ],
   imports: [
     BrowserModule
