85e0ec2a6c1e58eafac61ec624916959066fb299
DaeJin Jeong <devwithu@naver.com>
2017-08-31 12:24:13 +0900

    Page 345

--- simple-commerce-manager/src/app/scm-main/scm-main.module.ts
+++ simple-commerce-manager/src/app/scm-main/scm-main.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const CORE_COMPONENTS = [NavbarComponent, SidebarComponent, FooterComponent,
+  MainDashboardComponent, PageNotFoundComponent];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
-  exports: [NavbarComponent, SidebarComponent, FooterComponent]
+  imports: [CommonModule],
+  declarations: CORE_COMPONENTS,
+  exports: CORE_COMPONENTS
 })
 export class ScmMainModule { }
